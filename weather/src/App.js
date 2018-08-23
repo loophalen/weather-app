13df5c1c91773ce606875e7b394bd7004f56746c
blandhalen <blandhalen@blands-imac.home>
2018-08-22 21:23:29 -0400

    can display temperature

--- weather/src/App.js
+++ weather/src/App.js
@@ -7,20 +7,43 @@ const API_KEY = "c9060817f9148cc74205564669ddedcc"
 
 
 class App extends React.Component {
+  state = { 
+    temperature: undefined, 
+    city: undefined, 
+    country: undefined, 
+    humidity: undefined, 
+    description: undefined, 
+    error: undefined
+  }
   getWeather = async (e) => {
     e.preventDefault()
     const city = e.target.elements.city.value
     const country = e.target.elements.country.value
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`)
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`)
     const data = await api_call.json() 
     console.log(data)
+    this.setState({
+      temperature: data.main.temp, 
+      city: data.main, 
+      country: data.sys.country, 
+      humidity: data.main.humidity, 
+      description: data.weather[0].description, 
+      error: ""
+    })
   }
   render(){
     return(
       <div>
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather 
+        temperature={this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error={this.state.error}
+         />
       </div>
     )
   }
