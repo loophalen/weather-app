8c7512e40fb6764b8f4439a446ad2a8d004d3ebe
blandhalen <blandhalen@blands-imac.home>
2018-08-22 22:46:38 -0400

    removed error field broke app

--- weather/src/App.js
+++ weather/src/App.js
@@ -21,29 +21,16 @@ class App extends React.Component {
     const country = e.target.elements.country.value
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`)
     const data = await api_call.json() 
-    if(city && country){
-      console.log(data)
-      this.setState({
-        temperature: data.main.temp, 
-        city: data.main, 
-        country: data.sys.country, 
-        humidity: data.main.humidity, 
-        description: data.weather[0].description, 
-        error: ""
-      })
-    } else {
-      this.setState({
-        temperature: undefined, 
-        city: undefined,  
-        country: undefined, 
-        humidity: undefined, 
-        description: undefined, 
-        error: "Enter a city"
-      })
-    }
-
+    console.log(data)
+    this.setState({
+      temperature: data.main.temp, 
+      city: data.main, 
+      country: data.sys.country, 
+      humidity: data.main.humidity, 
+      description: data.weather[0].description, 
+      error: ""
+    })
   }
-   
   render(){
     return(
       <div>
--- weather/src/components/Weather.js
+++ weather/src/components/Weather.js
@@ -7,7 +7,6 @@ class Weather extends React.Component{
                {this.props.temperature &&  <p>Temperature: {this.props.temperature}</p>}
                {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
                {this.props.description && <p>Conditions: {this.props.description}</p>}
-               {this.props.error && <p>{this.props.error}</p>}
             </div>
         )
     }
