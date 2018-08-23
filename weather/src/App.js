3e97d9084c5e21be3a59cc901e26c0f19e00190b
blandhalen <blandhalen@blands-imac.home>
2018-08-22 20:53:06 -0400

    can fetch data

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import Titles from './components/Titles'
 import Form from './components/Form'
+import Weather from './components/Weather'
+
+const API_KEY = "c9060817f9148cc74205564669ddedcc"
+
 
 class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault()
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`)
+    const data = await api_call.json() 
+    console.log(data)
+  }
   render(){
     return(
       <div>
-      <Titles />
-      <Form />
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather />
       </div>
     )
   }
